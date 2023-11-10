7b4b0194d2a830093638941b297a47a9f653c863
mikel-js <michaelcastro.js@gmail.com>
2023-11-10 23:20:21 +0200

    add google fonts

--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -2,12 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 
-const StyledProfile = styled.div``;
+const StyledProfile = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 72rem;
+
+  > div {
+    flex: 1;
+  }
+`;
+const ImageContainer = styled.div``;
+const StyledText = styled.div``;
 
 const Profile = () => {
   return (
     <StyledProfile>
-      <Image src='/assets/pix.png' alt='profile-pic' width='407' height='613' />
+      <ImageContainer>
+        <Image
+          src='/assets/profile-pic.png'
+          alt='profile-pic'
+          width='407'
+          height='613'
+        />
+      </ImageContainer>
+      <StyledText>
+        <h1>Hi! I am Michael</h1>
+      </StyledText>
     </StyledProfile>
   );
 };
