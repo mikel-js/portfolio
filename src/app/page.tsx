0a7dc7cec76a7b4186ef2e084eacaba29e7a881f
mikel-js <michaelcastro.js@gmail.com>
2023-11-11 21:29:20 +0200

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import React from 'react';
+import Hero from './Hero';
 import Profile from './Profile';
 
 const page = () => {
   return (
     <div>
+      <Hero />
       <Profile />
     </div>
   );
